ca16920ca0330d429d199574f668fe06c37efed4
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 22:59:59 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -17,7 +17,13 @@ class Owner
   def buy_fish(name)
     self.pets[:fishes] << Fish.new(name)
   end
-
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
+  end
+  def buy_dog(name)
+    self.pets[:dogs] << Dog.new(name)
+  end
+  
   def sell_pets
     self.pets.each {|k, v| v.each {|pet| pet.mood = "nervous"}}
   end
