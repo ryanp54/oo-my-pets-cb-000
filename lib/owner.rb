139478b794908917d2e2f66463d45d3cfb4e6e96
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 22:45:34 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,12 @@
 class Owner
   @@all  # code goes here
+  attr_accessor :pets, :name
+  
+  def initialize(name)
+      @name = name
+      @pets = {fishes:[], cats: [], dogs:[]}
+      @species = []
+  end
 
   def self.all
     @@all
@@ -9,4 +16,9 @@ class Owner
     @@all.clear
   end
 
+  def self.count
+    @@all.size
+  end
+
+  
 end
